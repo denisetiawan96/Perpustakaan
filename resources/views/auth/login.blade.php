f5ec2923e2178ed345655e308522984c6ba3ba0a
denisetiawan96 <deni22setiawan@gmail.com>
2021-08-19 19:06:54 +0700

    front end

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="login-box">
+<div class="login-logo">
+    <a href="../../index2.html"><b>Admin</b>LTE</a>
+  </div>
+
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+
+      <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <div class="input-group-mb-3">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                 <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-envelope">
+                        </span>
+                      </div>
+                 </div>
+
+             @error('email')
+                 <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                 </span>
+             @enderror
+        </div>
+          <div class="input-group-mb-3">
+                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+             @error('password')
+                 <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong></span>
+             @enderror
+          </div>
+        <div class="row">
+          </div>
+          <!-- /.col -->
+
+
+
+          
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+@endsection
